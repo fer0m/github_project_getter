c30a2639c84c39138b570a776d8baa218e959286
as <as@freezingdata.de>
2024-05-03 11:11:10 +0200

    6. Ref settings

--- main/settings.py
+++ main/settings.py
@@ -65,7 +65,7 @@ TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
         'DIRS': [
-            os.path.join(BASE_DIR, 'templates'),  # Добавляете путь к общим шаблонам
+            os.path.join(BASE_DIR, 'templates'),
         ],
         'APP_DIRS': True,
         'OPTIONS': {
